f976fc4a0d818f0b5c2d57dc2465a0a947f11e87
Marpo13 <marlena.piotrowska1996@gmail.com>
2023-01-17 18:43:43 +0100

    Starting day only for employee

--- Backend/Wholesaler.Backend.Domain/UserService.cs
+++ Backend/Wholesaler.Backend.Domain/UserService.cs
@@ -36,6 +36,9 @@ namespace Wholesaler.Backend.Domain
             if (person == null)
                 throw new InvalidDataProvidedException($"There is no person with id: {userId}");
 
+            else if (person.Role != Role.Employee)
+                throw new InvalidDataProvidedException($"You can not create a workday for role: {person.Role}. You have to be an Employee.");
+
             var activeWorkday = _workdayRepository.GetActiveByPersonOrDefaultAsync(userId);
 
             if(activeWorkday != null)
